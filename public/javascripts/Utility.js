2055348f664910078e26b66ca0748c2a1df907e3
Flaherty <flahkj15@deacnet.wfu.edu>
2018-10-28 21:07:23 -0400

    Init Commit

--- public/javascripts/Utility.js
+++ public/javascripts/Utility.js
@@ -1,27 +1,28 @@
 let request = require('request');
 
-
-module.exports.grabHtml=function(link) {
-    request('https://news.wfu.edu/wp-json/wp/v2/posts', function (error, response, body) {
+//uses the request library to grab the data from the WP site.  It uses the request library and JSON parser to make sense of it
+module.exports.grabHtml = async function(link, callback) {
+    let demo = "Hello";
+    await request('https://news.wfu.edu/wp-json/wp/v2/posts', function(error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         //console.log('body:', JSON.parse(response.body)); // Print the HTML for the Google homepage.
         body = JSON.parse(body);
-        console.log(body[0]["id"])  //[0][whatever json field it is you want to access] https://stackoverflow.com/questions/42326078/access-json-fields-in-body-of-message
-
+        console.log((body[0]["id"])); //[0][whatever json field it is you want to access] https://stackoverflow.com/questions/42326078/access-json-fields-in-body-of-message
+        callback(link, body[0]["id"]);
 
     });
 
-    /*
-module.exports.reportHTML=function(link, data) {
-    if(data) {
-        return data
-    }
-    else {
-        grabHtml(link)
-    }
+
 };
-*/
 
 
+//function used to get the data from the callback function in request. Needed because return data within a call back
+module.exports.reportHTML = async function(link, data) {
+    if (data != null) {
+        console.log("Returning + " + data);
+        return data
+    } else {
+        await this.grabHtml(link, this.reportHTML)
+    }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
+let request = require('request');
+
 let express = require('express');
 let router = express.Router();
-let util  = require('../public/javascripts/Utility')
+let util  = require('../public/javascripts/Utility');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,13 +10,30 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/demo', function (req, res, next) {
-util.grabHtml(null)
-//console.log("Returned body: " + body)
-res.render('demo', {title: "Kappa"})
+
+    /*
+    To loop through and get all the news storied, run a loop get all unique ids, then
+    run the loop over [i]["content"].rendered as many times as necessary
+     */
+
+    //TODO: get this into git
+    //TODO: need to learn pug and see if we can make this thing more beauiful
+    //TODO: find a way out of async hell
+    request('https://news.wfu.edu/wp-json/wp/v2/posts', function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        body = JSON.parse(body);
+        res.render('demo', {title: body[0]["content"].rendered});  //to get the different articles [1][content].rendered, [2]....
+        //console.log((body[0]["id"])); //[0][whatever json field it is you want to access] https://stackoverflow.com/questions/42326078/access-json-fields-in-body-of-message
+    })
+
+    /*
+    good way of doing things
+    let body = util.reportHTML(link,null)
+    console.log("Returned body: " + body);
+    await res.render('demo', {title: body)})
+    */
 });
 
 module.exports = router;
 
-
-//TODO: Must get the data from grabhtml and pass that through to index.js.  Probably have some helper function or something
-//TODO: Put this project in git...share with Damian
